4a9f150438dcc5a931069626c1bc52f798208d6f
Rahul jain <rahul.jain@w3villa.com>
2022-08-19 17:57:01 +0530

    adding 2 post section and footer with responsive for all devices

---
+++ main.js
@@ -0,0 +1,11 @@
+const toggleBtn = document.querySelector('.toggle-btn');
+const toggle = document.querySelector('#toggle');
+console.log(toggle);
+
+
+console.log(toggleBtn);
+ collapse = document.querySelectorAll('.collapse');
+
+// toggleBtn.addEventListener('click', function(){
+//     console.log('collapse clicked')
+// })
